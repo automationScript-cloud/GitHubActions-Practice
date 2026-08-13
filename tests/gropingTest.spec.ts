da4bc293da85b62e0389325ee0f60d673d94735e
Jay Gupta <jaykumargupta095@gmail.com>
2026-08-13 20:07:22 +0530

    updated workflow

---
+++ tests/gropingTest.spec.ts
@@ -0,0 +1,21 @@
+import { test } from "@playwright/test";
+
+
+
+test("@Smoke Test1", async () => {
+    console.log("Test1");
+});
+
+test("@Smoke Test2", async () => {
+    console.log("Test2");
+});
+
+
+
+test("@regression Test3", async () => {
+    console.log("Test3");
+});
+
+test("@Smoke Test4", async () => {
+    console.log("Test4");
+});
